d81e3ff4c26e41f4c56c3d7889361806fd6fbea3
huy <nguyenlehuyuit@gmail.com>
2023-08-31 08:59:19 +0700

    add

--- src/App.vue
+++ src/App.vue
@@ -6,27 +6,66 @@ import {onBeforeMount, ref} from "vue";
 
 const socket = io('http://localhost:3001')
 const messages = ref([])
+const messageText = ref(null)
+const isJoined = ref(false)
+const name = ref(null)
+const isTyping = ref(false)
+const typingPerson = ref(null)
 
 onBeforeMount(() => {
     socket.emit('findAllMessages', {}, (res) => {
         messages.value = res
     })
 
-    socket.on('new message', {}, (res) => {
+    socket.on('new message', (res) => {
         messages.value.push(res)
     })
+
+  socket.on('typing', (res) => {
+    isTyping.value = res.isTyping
+    typingPerson.value = res.name
+  })
 })
 
-const sendMessage = (res) => {
-    socket.emit('createMessage', {text: messageText.value}, res =>{
-        messages.value.push(res)
+const typing = () => {
+  socket.emit('typing', {isTyping: true})
+  setTimeout(() => {
+    socket.emit('typing', {isTyping: false})
+  }, 2000)
+}
+
+const join = () => {
+  socket.emit('join', {name: name.value}, (res) => {
+    isJoined.value = true
+  })
+}
+
+const sendMessage = () => {
+    socket.emit('createMessage', {name:name.value, text:messageText.value}, res => {
+      messageText.value = null
     })
 }
 </script>
 
 <template>
 <div class="chat">
-    <div class="chat-container">
+  <div class="" v-if="!isJoined">
+    <form action="" @submit.prevent="join()">
+      <label for="">Name</label>
+      <input type="text" name="" id="" v-model="name">
+      <button type="submit">Join</button>
+    </form>
+  </div>
+
+    <div v-else class="chat-container">
+      <div v-if="isTyping">{{typingPerson}} is typing...</div>
+
+      <form @submit.prevent="sendMessage()" action="">
+        <label for="">New message</label>
+        <input type="text" name="" id="" v-model="messageText" @input="typing">
+        <button type="submit">Send</button>
+
+      </form>
         <div class="" v-for="message in messages">
             [{{message.name}}]: {{message.text}}
         </div>
